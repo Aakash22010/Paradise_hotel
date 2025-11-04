e6d90c2186bdb00c21a50da2668871ae483444a1
Aakash <aakashdahiya167@gmail.com>
2025-11-04 11:28:17 +0530

    Remove unique index on email field in user schema
    Removed index creation for unique email in user schema.

--- server/models/user.js
+++ server/models/user.js
@@ -13,8 +13,4 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   }
 });
-
-// Create index for faster queries
-userSchema.index({ email: 1 }, { unique: true });
-
 module.exports = mongoose.model('User', userSchema);
